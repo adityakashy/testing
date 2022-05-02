9fea89704c8b31afec9add4607228f9d9dd8a50a
Aditya <aditya.kashyap@kelltontech.com>
2022-05-02 14:13:57 +0530

    testing

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+
+import Counter from './Counter';
+import User from './User';
+
+function App() {
+
+  const data = {
+    userDetails: [
+      { name: 'John', age: 24, id: 1234 },
+      { name: 'Mary', age: 29, id: 1289 },
+      { name: 'Joel', age: 27, id: 1567 }
+    ]
+  }
+  return (
+    <div className="App">
+      <Counter />
+      <User data={data} />
+    </div>
+  );
+}
+
+export default App;
